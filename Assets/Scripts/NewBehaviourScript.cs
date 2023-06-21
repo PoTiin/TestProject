7ee0f023d09dcd2d9c79ac8fd3900621ad48cc76
PoTiin <573031461@qq.com>
2023-06-21 15:41:43 +0800

    第二次提交测试

--- Assets/Scripts/NewBehaviourScript.cs
+++ Assets/Scripts/NewBehaviourScript.cs
@@ -4,7 +4,7 @@ using UnityEngine;
 
 public class NewBehaviourScript : MonoBehaviour
 {
-    public int ID = 0;
+    public int ID = 2;
     // Start is called before the first frame update
     void Start()
     {
